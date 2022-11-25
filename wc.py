40d8133bd325d47f719e6452731c40dec6adc761
shigeokatakeshi <takeshi0713@gmail.com>
2022-11-25 22:32:02 +0900

    中身完成

--- wc.py
+++ wc.py
@@ -23,7 +23,7 @@ hkd = float(df.iloc[-1, 15])
 
 l_currency = [usd, eur, gbp, cny, aud, cad, chf, hkd]
 
-# 世界通貨シェアを200で割って％として数値を作成
+# wc_propoで作成したデータフレームの世界通貨シェアを200で割って％として数値を作成
 p_usd = (df3.iloc[-1, 0]) / 200
 p_eur = (df3.iloc[-1, 1]) / 200
 p_jpy = (df3.iloc[-1, 2]) / 200
@@ -39,21 +39,21 @@ p_etc = (df3.iloc[-1, 9]) / 200
 p_wc = [p_usd, p_eur, p_jpy, p_gbp, p_cny, p_aud, p_cad, p_chf, p_hkd, p_etc]
 
 # その他を計算する単位を作る為、取り出した通貨の最小単位を合計して100で割る
-wc_min = (
-    float(usd)
-    + float(eur)
-    + float(usd)
-    + float(gbp)
-    + float(cny)
-    + float(aud)
-    + float(cad)
-    + float(chf)
-    + float(hkd)
-)
-wc_min = wc_min / 9
+# wc_min = (
+#     float(usd)
+#     + float(eur)
+#     + float(usd)
+#     + float(gbp)
+#     + float(cny)
+#     + float(aud)
+#     + float(cad)
+#     + float(chf)
+#     + float(hkd)
+# )
+# wc_min = wc_min / 9
 
-# 更に通貨シェアを掛けた数値をwcとする。
-wc = (
+# 各通貨シェアを掛けた数値をetcの係数とする。
+etc = (
     (p_usd * float(usd))
     + (p_eur * float(eur))
     + (p_jpy * float(usd))
@@ -63,12 +63,11 @@ wc = (
     + (p_cad * float(cad))
     + (p_chf * float(chf))
     + (p_hkd * float(hkd))
-    + (p_etc * float(wc_min))
+    + (p_etc * float(usd))
 )
 
-# wcを考え直し、令和4年11月22日の10円を、FXレートと世界シェアで1wcとして決める
-# 計算すると「0.9375038685614582」だったので切りよく11月22日の10円が1wcとする
-# 1wcの内訳は「0.039403321137067265, 0.010636837771067801, 0.8500000000000001, 0.0038605452277721683, 0.017658930373360245, 0.0031904711262363073, 0.0028360748723766306, 0.001684976747320887, 0.00823271130625686]
+# wcの基準 令和4年11月22日の10円を、FXレートと世界シェアで1wcとして決める
+# 1wcの内訳は「0.039403321137067265usd 0.010636837771067801eur 0.8500000000000001jpy 0.0038605452277721683gbp 0.017658930373360245, 0.0031904711262363073, 0.0028360748723766306, 0.001684976747320887, 0.00823271130625686]
 
 b_usd = (10 * p_usd) / 142.12
 b_eur = (10 * p_eur) / 145.72
@@ -83,7 +82,8 @@ b_etc = (10 * p_etc) / 142.12
 
 # その他分のシェアはドルのシェアに合計しておく
 b_usd = b_usd + b_etc
-# 各通貨のバラバラな単位なので、当日のレートを掛け、円に戻して合計する
+
+# wcの原価を計算するため各通貨の当日のレートを掛け、円に戻して合計し、wcの現在価格を出す
 b_wc = (
     (b_usd * usd)
     + (b_eur * eur)
@@ -96,7 +96,7 @@ b_wc = (
     + (b_hkd * hkd)
 )
 
-b_wc_list = [b_usd, b_eur, b_jpy, b_gbp, b_cny, b_aud, b_cad, b_chf, b_hkd]
+# b_wc_list = [b_usd, b_eur, b_jpy, b_gbp, b_cny, b_aud, b_cad, b_chf, b_hkd]
 
 # トップページへ
 @app.route("/")
@@ -104,66 +104,67 @@ def index():
     return render_template("index.html")
 
 
-# 円が選択され数値が入力された時
+# 数値が入力された時
 @app.route("/measurement", methods=["GET", "POST"])
 def fx_currency():
     if request.method == "POST":
         currency = request.form["currency1"]
-        if currency == "JPY":
+
+        # 円以外が選択されると各通貨の為替レートを掛ける
+        if currency == "円":
             i_jpy = int(request.form["number"])
-            usd1 = (i_jpy * p_usd) / usd
-            eur1 = (i_jpy * p_eur) / eur
-            jpy1 = (i_jpy * p_jpy) / jpy
-            gbp1 = (i_jpy * p_gbp) / gbp
-            cny1 = (i_jpy * p_cny) / cny
-            aud1 = (i_jpy * p_aud) / aud
-            gbp1 = (i_jpy * p_gbp) / gbp
-            cad1 = (i_jpy * p_cad) / cad
-            chf1 = (i_jpy * p_chf) / chf
-            hkd1 = (i_jpy * p_hkd) / hkd
-            etc1 = (i_jpy * p_etc) / wc
+        if currency == "ドル":
+            i_jpy = int(request.form["number"])
+            i_jpy = i_jpy * usd
+        if currency == "ユーロ":
+            i_jpy = int(request.form["number"])
+            i_jpy = i_jpy * eur
+        # 入力した金額にシェア割を掛けて、為替で割る
+        usd1 = (i_jpy * p_usd) / usd
+        eur1 = (i_jpy * p_eur) / eur
+        jpy1 = (i_jpy * p_jpy) / jpy
+        gbp1 = (i_jpy * p_gbp) / gbp
+        cny1 = (i_jpy * p_cny) / cny
+        aud1 = (i_jpy * p_aud) / aud
+        gbp1 = (i_jpy * p_gbp) / gbp
+        cad1 = (i_jpy * p_cad) / cad
+        chf1 = (i_jpy * p_chf) / chf
+        hkd1 = (i_jpy * p_hkd) / hkd
+        etc1 = (i_jpy * p_etc) / etc
 
-            # 入力された金額をwcの基準価格で割りwc建ての価値を出す
-            wcjpn = i_jpy / b_wc
+        # 入力された金額をwcの基準価格で割りwc建ての価値を出す
+        wcjpn = i_jpy / b_wc
 
-            jpy_wc = [
-                round(usd1),
-                round(eur1),
-                round(jpy1),
-                round(gbp1),
-                round(cny1),
-                round(aud1),
-                round(cad1),
-                round(chf1),
-                round(hkd1),
-                round(etc1),
-                round(wcjpn),
-            ]
-            print(b_wc_list)
-            print(wcjpn)
-            # print(df.iloc[-1, 1])
-            fx_currency = jpy_wc
+        # 入力した値に各通貨シェアの％を掛けて算出した値の小数点を削除しリスト化
+        fx_currency = [
+            round(usd1),
+            round(eur1),
+            round(jpy1),
+            round(gbp1),
+            round(cny1),
+            round(aud1),
+            round(cad1),
+            round(chf1),
+            round(hkd1),
+            round(etc1),
+            round(wcjpn),
+        ]
 
-        # elif currency == "USD":
-        #     fx_currency = fx_currency.USD()
+        # 結果ページの表示を入力画面と合わせるために再計算
+        if currency == "ドル":
+            i_jpy = i_jpy / usd
+            i_jpy = round(i_jpy)
+        if currency == "ユーロ":
+            i_jpy = i_jpy / eur
+            i_jpy = round(i_jpy)
 
-        return render_template(
-            "measurement.html",
-            fx_currency=fx_currency,
-            i_jpy=i_jpy,
-            currency=currency,
-            l_currency=l_currency,
-            # usd=usd,
-            # eur=eur,
-            # gbp=gbp,
-            # cny=cny,
-            # aud=aud,
-            # cad=cad,
-            # chf=chf,
-            # hkd=hkd,
-            # wcjpn=wcjpn,
-            b_wc_list=b_wc_list,
-        )
+    return render_template(
+        "measurement.html",
+        fx_currency=fx_currency,
+        i_jpy=i_jpy,
+        currency=currency,
+        l_currency=l_currency,
+    )
 
     return redirect("/")
 
